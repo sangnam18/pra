133622a061b5c6956cccd7038a938da32730ed01
sangnam18 <sangnam18@nate.com>
2014-08-15 19:14:50 +0900

    ㄴㅇㄹ

--- src/main/java/min/ca/App.java
+++ src/main/java/min/ca/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "adfasdHello World! be2" );
+        System.out.println( "adfasdHello World! be2 서버 2번째 올림" );
     }
 }
