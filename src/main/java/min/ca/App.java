001ec2181dd62267ef3671126354290b91200ffd
sangnam18 <sangnam18@nate.com>
2014-08-15 19:13:20 +0900

    브랜치1 커밋1

--- src/main/java/min/ca/App.java
+++ src/main/java/min/ca/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "adfasdHello World!" );
+        System.out.println( "adfasdHello World! be2" );
     }
 }
